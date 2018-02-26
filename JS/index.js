4a90b24c70da502da141beb31606799baf2e2228
leobaltazor <leo.baltazor@gmail.com>
2018-02-26 21:32:01 +0200

    2

---
+++ JS/index.js
@@ -0,0 +1,146 @@
+// var number1 = 14;
+// var number2 = 98.6;
+// var number3 = -14;
+// var number4 = NaN;
+// var number5 = Infinity;
+// var und;
+
+// //  + sum
+// var summ = number1 + number2;
+
+// // 	-
+// var minus = number1 - number2;
+
+// // 	/
+// var dele = number1 / number2;
+
+// // 	*
+// var mult = number1 * number2;
+
+
+// var naan = number1 / "d";
+
+// console.log(summ, minus, dele, mult, naan);
+// console.log(und);
+
+	//zadanie 1
+
+// var x = 70,
+// 	y = -20;
+
+// // var rez = x;
+// // x = y;
+// // y = rez;
+// console.log("X " + x, " Y " + y);
+
+// x = x + y;
+// y = x - y;
+// x = x - y;
+
+// console.log("X " + x," Y " + y);
+
+//secont
+
+// var p1 = "Hello world",
+// 	p2 = "Chupryna Oleksii";
+
+// console.log(p1 + ' " " ' + p2)
+
+// var p1 = `Так задается многострочный 
+// комментарий /* текст */`
+// console.log(p1)
+
+
+// //ШАБЛОННЫЙ ТЕКСТ
+// var a = 3,
+// 	c = 6;
+
+// var text = `a+c=${a+c}`;
+// // console.log(text);
+
+// var p1 = "Oleksii",
+// 	p2 = "HTML",
+// 	p3 = "CSS",
+// 	p4 = "JavaScript",
+// 	p5 = "Photoshop";
+// var p = ["CSS", "HTML"];
+// 	var text = `My name is ${p1}`;
+// console.log(text);
+// for (var i = 0; i < p.length; i++) {
+
+// 	var text = `Я изучаю ${p[i]}`;
+// console.log(text);
+// } 
+// 	var text = `My name is ${p1}`;
+// console.log(text);
+// 	var text = `Я изучаю ${p2}`;
+// console.log(text);
+// 	var text = `Я изучаю ${p3}`;
+// console.log(text);
+// 	var text = `Я изучаю ${p4}`;
+// console.log(text);
+// 	var text = `Я изучаю ${p5}`;
+// console.log(text);
+
+// var num = "1000";
+// console.log(num + 1);
+
+// var testbool = true;
+// var testbool = false;
+// console.log(testbool);
+
+
+// var x;
+// // x=1;
+// console.log(x)
+
+// var p1 = 10,
+// 	p2 = "test1",
+// 	p3 = true;
+
+// console.log(typeof p1, typeof p2, typeof p3)
+
+// var a = 1, 
+// 	b = typeof a;
+// console.log(typeof b)
+
+// var a = 0;
+// console.log(--a);
+// console.log(a--);
+// console.log(a);
+
+// var a = 0;
+// var b = 10;
+// var text = `
+// a+=b:${a+=b}, 
+// a-=b:${a-=b}, 
+// a*=b:${a*=b}, 
+// a/=b:${a/=b},
+// a%=b:${a%=b}`
+// console.log(text)
+
+// var t_temp = 20;
+// z_temp = 30;
+
+// if (t_temp<z_temp) {
+// 	console.log("включить")
+// } else {
+// 	console.log("не нужно вкл")
+// // }
+
+// var x = 10,
+// 	a = 1,
+// 	b = 2,
+// 	c = 3,
+// 	d = 0;
+
+// // a*x*x+b*x+c=null
+// d=b*b-4*a*c
+
+
+
+
+// console.log(d)
+
+var a = "1";
+console.log(a == 1);
